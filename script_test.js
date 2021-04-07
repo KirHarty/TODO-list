95d623f384983556e49f018b27b5739e08417d66
KirHarty <sanekpuhckov27@gmail.com>
2021-04-07 16:10:14 +0300

    chenges

--- script_test.js
+++ script_test.js
@@ -1,25 +1,50 @@
-  let addMessage = document.querySelector('.message'),
-   addButton = document.querySelector('.add');
-    todo =    document.querySelector('.todo');
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add');
+todo =    document.querySelector('.todo');
 
-  let todoList = [];
+let todoList = [];
 
-  addButton.addEventListener('click',function () {
+addButton.addEventListener('click',function () {
 
-      let newTodo = {
-          todo: addMessage.value,
-          checked: false,
-          important: false
-      };
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
 
-      todoList.push(newTodo);
-      displayMessages();
-  });
+    todoList.push(newTodo);
+    displayMessages();
+ });
 
-  function displayMessages() {
+ function displayMessages() {
+    let displayMessage = '';
+    todoList.forEach(function (item, i){
+        displayMessage += `
+        <li>
+           <input type="checkbox" id='item_${i}' ${item.checked ? 'checked' : null}> 
+           <label for='item_${i}' class="${item.important ? 'important' : ''}" >${item.todo}</label>
+       </li>
+       `;
+        todo.innerHTML = displayMessage;
+    });
+ }
 
+ todo.addEventListener('change',function (event){
+    let idInput = event.target.getAttibute('id');
 
-      todoList.forEach(function (item, index) {
-          console.log(item);
-      });
-  } 
+    todoList.forEach(function (item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo',JSON.stringify(todoList));
+        }
+    })
+ });
+
+ todo.addEventListener('contextmenu', function (event){
+   event.preventDefault();
+   todoList.forEach(function (item){
+       if (item.todo === event.target.innerHTML) {
+           item.important = !item.important;
+       }
+   });
+ });
