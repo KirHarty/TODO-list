fbf178606ec591ec943ec01dd41ba6b3d6dbbe60
KirHarty <sanekpuhckov27@gmail.com>
2021-04-08 16:29:05 +0300

    'chenges'

--- script_test.js
+++ script_test.js
@@ -1,6 +1,6 @@
 /**
  * 1) Добавить функцию window onload
- * 2) Написать функцию render и поместить её в window onload
+ * 2) Написать функцию render(имя) и поместить её в window onload
  * 3) вынести все addEventListener в отдельную функцию и вызвать после render
  * 4) доработать addEventListener, вынести функцию отдельно от самого addEventListener
  * 5) displayMessages переимновать в renderTodo и переделать в рендер 1 туду 'append() или appendChild()'
@@ -8,56 +8,79 @@
  * 7) функция удаления to-do из списка с обновлением HTML
  * */
 
+function subscribe (elements, action) {
 
-let addMessage = document.querySelector('.message');
-let addButton = document.querySelector('.add');
-let todo = document.querySelector('.todo');
-let todoList = [];
+    function addTodo() {
+        const newTodo = {
+            todo: elements.addMessage.value,
+            checked: false,
+            important: false
+        };
 
-addButton.addEventListener('click',function () {
+        //todoList.push(newTodo);
+    }
 
-    let newTodo = {
-        todo: addMessage.value,
-        checked: false,
-        important: false
-    };
+    if (action === 'subscribe') {
+        elements.addButton.addEventListener('click', addTodo);
+    } else if (action === 'unsubscribe') {
+        elements.addButton.removeEventListener('click', addTodo);
+    }
+}
 
-    todoList.push(newTodo);
-    displayMessages();
- });
+function render() {
+    let addMessage = document.querySelector('.message');
+    let addButton = document.querySelector('.add');
+    let todo = document.querySelector('.todo');
+    let todoList = [];
 
- function displayMessages() {
-    let displayMessage = '';
-    todoList.forEach(function (item, index){
-        displayMessage += `
+    subscribe({addMessage,addButton,todo}, 'subscribe');
+
+
+
+
+    function displayMessages() {
+        let displayMessage = '';
+        todoList.forEach(function (item, index){
+            displayMessage += `
         <li>
            <input type="checkbox" id='item_${index}' ${item.checked ? 'checked' : null}> 
            <label for='item_${index}' class="${item.important ? 'important' : null}" >${item.todo}</label>
        </li>`;
-        todo.innerHTML = displayMessage;
-    });
- }
-
- todo.addEventListener('change',function (event){
-    // let idInput = event.target.getAttibute('id');
-
-    console.log("tut", event.target)
-    todoList.forEach(function (item){
-        if (item.todo === valueLabel){
-            item.checked = !item.checked;
-            localStorage.setItem('todo',JSON.stringify(todoList));
-        }
-    })
- });
-
- todo.addEventListener('contextmenu', function (event){
-   event.preventDefault();
-   todoList.forEach(function (item){
-       if (item.todo === event.target.innerHTML) {
-           item.important = !item.important;
-       }
-   });
- });
+            todo.innerHTML = displayMessage;
+        });
+    }
 
 
 
+}
+
+
+window.onload = function() {
+    console.log('hello')
+    render();
+};
+function Event() {
+
+}
+
+
+//    elements.todo.addEventListener('change',function (event){
+//         // let idInput = event.target.getAttibute('id');
+//
+//         console.log("tut", event.target)
+//         todoList.forEach(function (item){
+//             if (item.todo === valueLabel){
+//                 item.checked = !item.checked;
+//                 localStorage.setItem('todo',JSON.stringify(todoList));
+//             }
+//         })
+//     });
+//
+//     elements.todo.addEventListener('contextmenu', function (event){
+//         event.preventDefault();
+//         todoList.forEach(function (item){
+//             if (item.todo === event.target.innerHTML) {
+//                 item.important = !item.important;
+//             }
+//         });
+//     });
